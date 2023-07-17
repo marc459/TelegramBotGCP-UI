761a822b715813117ead04614b9976f0522217d8
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2023-07-18 01:08:54 +0200

    react works again

--- ui-service/test-app/src/App.js
+++ ui-service/test-app/src/App.js
@@ -1,52 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState('');
-  // const socketRef = useRef(null);
-
-  // useEffect(() => {
-  //   // Connect to the WebSocket server
-  //   socketRef.current = new WebSocket('ws://bot-service-36gz5wrlea-ue.a.run.app');
-
-  //   // Handle incoming messages
-  //   socketRef.current.onmessage = (event) => {
-  //     const message = event.data;
-  //     setMessages((prevMessages) => [...prevMessages, message]);
-  //   };
-
-  //   // Clean up the WebSocket connection on unmount
-  //   return () => {
-  //     socketRef.current.close();
-  //   };
-  // }, []);
-
-  // const handleInputChange = (event) => {
-  //   setInputValue(event.target.value);
-  // };
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-
-  //   if (inputValue.trim()) {
-  //     // Send the message to the server
-  //     socketRef.current.send(inputValue);
-  //     setInputValue('');
-  //   }
-  // };
-
   return (
-    <div>
-      <h1>Movistar Client Chat</h1>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>{message}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button type="submit">Send</button>
-      </form>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
